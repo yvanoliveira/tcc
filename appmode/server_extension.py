0674edc7b9689cf63f78f5d7bee9574d05bc86a5
Yvan Oliveira <85844516+yvanoliveira@users.noreply.github.com>
2023-02-28 11:58:18 -0300

    Create appmode folder

---
+++ appmode/server_extension.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+import os
+import itertools
+from notebook.utils import url_path_join
+from notebook.base.handlers import IPythonHandler, FilesRedirectHandler, path_regex
+import notebook.notebook.handlers as orig_handler
+from tornado import web, gen
+from traitlets.config import LoggingConfigurable
+from traitlets import Bool, Unicode
+
+
+class Appmode(LoggingConfigurable):
+    """Object containing server-side configuration settings for Appmode.
+    Defined separately from the AppmodeHandler to avoid multiple inheritance.
+    """
+    trusted_path = Unicode('', help="Run only notebooks below this path in Appmode.", config=True)
+    show_edit_button = Bool(True, help="Show Edit App button during Appmode.", config=True)
+    show_other_buttons = Bool(True, help="Show other buttons, e.g. Logout, during Appmode.", config=True)
+
+#===============================================================================
+class AppmodeHandler(IPythonHandler):
+    @property
+    def trusted_path(self):
+        return self.settings['appmode'].trusted_path
+
+    @property
+    def show_edit_button(self):
+        return self.settings['appmode'].show_edit_button
+
+    @property
+    def show_other_buttons(self):
+        return self.settings['appmode'].show_other_buttons
+
+    #===========================================================================
+    @web.authenticated
+    def get(self, path):
+        """get renders the notebook template if a name is given, or
+        redirects to the '/files/' handler if the name is not given."""
+
+        path = path.strip('/')
+        self.log.info('Appmode get: %s', path)
+
+        # Abort if the app path is not below configured trusted_path.
+        if not path.startswith(self.trusted_path):
+            self.log.warn('Appmode refused to launch %s outside trusted path %s.', path, self.trusted_path)
+            raise web.HTTPError(401, 'Notebook is not within trusted Appmode path.')
+
+        cm = self.contents_manager
+
+        # will raise 404 on not found
+        try:
+            model = cm.get(path, content=False)
+        except web.HTTPError as e:
+            if e.status_code == 404 and 'files' in path.split('/'):
+                # 404, but '/files/' in URL, let FilesRedirect take care of it
+                return FilesRedirectHandler.redirect_to_files(self, path)
+            else:
+                raise
+        if model['type'] != 'notebook':
+            # not a notebook, redirect to files
+            return FilesRedirectHandler.redirect_to_files(self, path)
+
+        # fix back button navigation
+        self.add_header("Cache-Control", "cache-control: private, max-age=0, no-cache, no-store")
+
+        # gather template parameters
+        tmp_path = self.mk_tmp_copy(path)
+        tmp_name = tmp_path.rsplit('/', 1)[-1]
+        render_kwargs = {
+            'notebook_path': tmp_path,
+            'notebook_name': tmp_name,
+            'kill_kernel': False,
+            'mathjax_url': self.mathjax_url,
+            'mathjax_config': self.mathjax_config,
+            'show_edit_button': self.show_edit_button,
+            'show_other_buttons': self.show_other_buttons,
+        }
+
+        # template parameters changed over time
+        for parameter in ("get_custom_frontend_exporters", "get_frontend_exporters"):
+            if hasattr(orig_handler, parameter):
+                render_kwargs[parameter] = getattr(orig_handler, parameter)
+
+        # Ok let's roll ....
+        self.write(self.render_template('appmode.html', **render_kwargs))
+
+    #===========================================================================
+    @web.authenticated
+    @gen.coroutine
+    def post(self, path):
+        assert self.get_body_arguments("appmode_action")[0] == "delete"
+        path = path.strip('/')
+        self.log.info('Appmode deleting: %s', path)
+
+        # delete session, including the kernel
+        sm = self.session_manager
+        if gen.is_coroutine_function(sm.get_session):
+            s = yield sm.get_session(path=path)
+        else:
+            s = sm.get_session(path=path)
+        if gen.is_coroutine_function(sm.delete_session):
+            yield sm.delete_session(session_id=s['id'])
+        else:
+            sm.delete_session(session_id=s['id'])
+
+        # delete tmp copy
+        cm = self.contents_manager
+        cm.delete(path)
+        self.finish()
+
+    #===========================================================================
+    def mk_tmp_copy(self, path):
+        cm = self.contents_manager
+
+        # find tmp_path
+        dirname = os.path.dirname(path)
+        fullbasename = os.path.basename(path)
+        basename, ext = os.path.splitext(fullbasename)
+        for i in itertools.count():
+            tmp_path = "%s/.%s-%i%s"%(dirname, basename, i, ext)
+            if not cm.exists(tmp_path):
+                break
+
+        # create tmp copy - allows opening same notebook multiple times
+        self.log.info("Appmode creating tmp copy: "+tmp_path)
+        cm.copy(path, tmp_path)
+
+        return(tmp_path)
+
+#===============================================================================
+def load_jupyter_server_extension(nbapp):
+    tmpl_dir = os.path.dirname(__file__)
+    # does not work, because init_webapp() happens before init_server_extensions()
+    #nbapp.extra_template_paths.append(tmpl_dir) # dows
+
+    # For configuration values that can be set server side
+    appmode = Appmode(parent=nbapp)
+    nbapp.web_app.settings['appmode'] = appmode
+
+    # slight violation of Demeter's Law
+    rootloader = nbapp.web_app.settings['jinja2_env'].loader
+    for loader in getattr(rootloader, 'loaders', [rootloader]):
+        if hasattr(loader, 'searchpath') and tmpl_dir not in loader.searchpath:
+            loader.searchpath.append(tmpl_dir)
+
+    web_app = nbapp.web_app
+    host_pattern = '.*$'
+    route_pattern = url_path_join(web_app.settings['base_url'], r'/apps%s' % path_regex)
+    web_app.add_handlers(host_pattern, [(route_pattern, AppmodeHandler)])
+
+    if appmode.trusted_path:
+        nbapp.log.info("Appmode server extension loaded with trusted path: %s", appmode.trusted_path)
+    else:
+        nbapp.log.info("Appmode server extension loaded.")
+
+#EOF
---
+++ appmode/static/main.js
@@ -0,0 +1,198 @@
+define([
+    'jquery',
+    'base/js/namespace',
+    'base/js/dialog',
+    'base/js/events',
+    'base/js/utils',
+    'require',
+], function(
+    $,
+    Jupyter,
+    dialog,
+    events,
+    utils,
+    require
+) {
+    "use strict";
+
+    //==========================================================================
+    var get_cookie = function (name) {
+        // from tornado docs: http://www.tornadoweb.org/en/stable/guide/security.html
+        var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+        return r ? r[1] : undefined;
+    }
+
+    //==========================================================================
+    var appmode_unload_handler = function (e) {
+        var nb = Jupyter.notebook;
+        var url_parts = [nb.base_url, 'apps', nb.notebook_path];
+        var url = utils.url_path_join.apply(null, url_parts);
+
+        // tell server to clean up session, kernel, and tmp notebook file.
+        var form_data = new FormData();
+        form_data.append("appmode_action", "delete");
+        form_data.append("_xsrf", get_cookie("_xsrf"));
+        navigator.sendBeacon(url, form_data);
+    };
+
+    //==========================================================================
+    var kernel_busy_handler = function (e) {
+        $('#appmode-busy').css('visibility', 'visible');
+    }
+
+    //==========================================================================
+    var kernel_idle_handler = function (e) {
+        $('#appmode-busy').css('visibility', 'hidden');
+    }
+
+    //==========================================================================
+    function goto_app_mode() {
+        // kill Jupyter session
+        Jupyter.notebook.session.delete();
+
+        // build new URL
+        var base_url = Jupyter.notebook.base_url;
+        var prefix = base_url+"notebooks/"
+        var path = window.location.pathname.substring(prefix.length);
+        var search = window.location.search;
+        var scroll = "appmode_scroll=" + $('#site').scrollTop();
+        if (search.search(/appmode_scroll=\d+/) != -1){
+            search = search.replace(/appmode_scroll=\d+/g, scroll);
+        }else if (search.length == 0) {
+            search = "?" + scroll;
+        } else {
+            search += "&" + scroll;
+        }
+
+        // goto new URL
+        window.location.href = base_url + "apps/" + path + search;
+    }
+
+    //==========================================================================
+    function goto_normal_mode() {
+        // build new URL
+        var base_url = Jupyter.notebook.base_url;
+        var prefix = base_url+"apps/"
+        var path = window.location.pathname.substring(prefix.length);
+        var new_url = base_url + "notebooks/" + path + window.location.search;
+
+        // goto new URL
+        // Not using location.pathname as it might urlencode the path again
+        window.location.href = new_url;
+    }
+
+    //==========================================================================
+    function kernel_inject_url(kernel) {
+        kernel.execute("jupyter_notebook_url = '" + window.location + "'");
+    }
+
+    //==========================================================================
+    function initialize_step1() {
+        if (Jupyter.notebook && Jupyter.notebook._fully_loaded) {
+            initialize_step2();
+        }else{
+            events.one('notebook_loaded.Notebook', initialize_step2);
+        }
+    }
+
+    //==========================================================================
+    function initialize_step2() {
+        // scroll to last position if in normal mode
+        var base_url = Jupyter.notebook.base_url;
+        if(window.location.pathname.startsWith(base_url+"notebooks/")){
+             var url = window.location.href;
+             var m = url.match(/appmode_scroll=(\d+)/);
+             if(m)
+                $('#site').scrollTop(m[1]);
+        }
+
+        var nb = Jupyter.notebook;
+        if(nb.kernel && nb.kernel.info_reply.status) {
+            initialize_step3();
+        }else{
+            events.one('kernel_ready.Kernel', initialize_step3);
+        }
+    }
+
+    //==========================================================================
+    function initialize_step3() {
+        kernel_inject_url(Jupyter.notebook.kernel);
+
+        // run kernel_inject_url() again when a new kernel is created
+        events.on('kernel_created.Kernel kernel_created.Session', function(event, data) {
+            kernel_inject_url(data.kernel);
+        });
+
+        var base_url = Jupyter.notebook.base_url;
+        if(window.location.pathname.startsWith(base_url+"apps/")){
+            // appmode is active -> continue intializtion
+            initialize_step4();
+        }
+    }
+
+    //==========================================================================
+    function initialize_step4() {
+        // install unload-handler
+        window.addEventListener('unload', appmode_unload_handler, false);
+
+        // disable autosave
+        Jupyter.notebook.set_autosave_interval(0);
+
+        // disable keyboard shortcuts
+        Jupyter.keyboard_manager.command_shortcuts.clear_shortcuts();
+        Jupyter.keyboard_manager.edit_shortcuts.clear_shortcuts();
+
+        // disable editing of text cells
+        $('.text_cell').off("dblclick");
+
+        // disable editing of raw cells
+        $('.CodeMirror').each(function() {
+            this.CodeMirror.setOption('readOnly', "nocursor");
+        });
+
+        // register kernel events
+        events.on('kernel_idle.Kernel',  kernel_idle_handler);
+        events.on('kernel_busy.Kernel',  kernel_busy_handler);
+
+        // register on_click handler
+        $('#appmode-leave').click(goto_normal_mode);
+
+        // clear all cell outputs persisted in the initial notebook
+        Jupyter.notebook.clear_all_output();
+        // try to load the jupyter-js-widgets extension to see if it's installed
+        utils.load_extension('jupyter-js-widgets/extension').then(function(module) {
+            // force the extension to initialize if it exists, even if it's a repeat
+            // operation, to avoid a race condition with executing cells that contain
+            // widgets
+            module.load_ipython_extension().then(initialize_step5);
+        }).catch(initialize_step5);
+    }
+
+    //==========================================================================
+    function initialize_step5() {
+        // hide loading screen
+        $('#appmode-loader').slideUp();
+        // execute all cells
+        Jupyter.notebook.execute_all_cells();
+        console.log("Appmode: initialization finished");
+    };
+
+    //==========================================================================
+    var load_ipython_extension = function() {
+
+        // add button to toolbar in case appmode is not enabled
+        Jupyter.toolbar.add_buttons_group([{
+            id : 'toggle_codecells',
+            label : 'Appmode',
+            icon : 'fa-arrows-alt',
+            callback : goto_app_mode
+        }]);
+
+        initialize_step1();
+    };
+
+    //==========================================================================
+    return {
+        load_ipython_extension : load_ipython_extension
+    };
+});
